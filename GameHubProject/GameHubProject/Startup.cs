505dafd4bb71cf8ab6d71e8112b686238210d3a6
longblue2910 <tienlong291099@gmail.com>
2020-11-17 10:25:01 +0700

    Long add identity user

--- GameHubProject/GameHubProject/Startup.cs
+++ GameHubProject/GameHubProject/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GameHub.API.DbContext;
+using GameHub.Domain;
+using GameHub.Domain.User;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace GameHubProject
 {
@@ -26,6 +24,8 @@ namespace GameHubProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Common.ConnectionString));
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
